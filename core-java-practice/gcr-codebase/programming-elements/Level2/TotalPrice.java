2e7e245588d41f2db398d33e0231a567c35244b8
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2025-12-18 15:55:18 +0530

    Level 2 Completed

---
+++ core-java-practice/gcr-codebase/programming-elements/Level2/TotalPrice.java
@@ -0,0 +1,21 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class TotalPrice {
+       public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double total=0;
+        //taking the input
+        System.out.println("Enter the price and quatity to be baught");
+        double p=sc.nextInt();
+        int q=sc.nextInt();
+
+        //Calculating the total price
+        total=p*q;
+
+        System.out.println("The total purchase price is INR "+total+" if the quantity "+p+" and unit price is INR "+q);
+
+        sc.close();
+    }   
+}
