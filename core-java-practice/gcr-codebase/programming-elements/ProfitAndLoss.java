2c0c1b32fc0ffe71ed9fd716d48347e932d82820
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2025-12-18 12:04:18 +0530

    4 Programs completed

---
+++ core-java-practice/gcr-codebase/programming-elements/ProfitAndLoss.java
@@ -0,0 +1,16 @@
+public class ProfitAndLoss {
+    public static void main(String[] args) {
+        double CP=129;
+        double SP=191;
+
+        //Calculating profit
+        double profit= SP-CP;
+
+        //Calculating profit percentage
+        double percentage=profit*100;
+
+        System.out.println("Profit calculated is = "+profit);
+        System.out.println("Profit percentage calculated is = "+percentage);
+
+    }
+}
