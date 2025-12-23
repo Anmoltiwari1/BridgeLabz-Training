6a3f6c24f28cb163ad9f2585d5b6f8d8e293dd64
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2025-12-23 10:16:52 +0530

    Create FactorsFunction.java

---
+++ core-java-practice/gcr-codebase/Methods/FactorsFunction.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FactorsFunction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+    }
+
+    public static ArrayList<Integer> factors(int n){
+        
+    }
+}
