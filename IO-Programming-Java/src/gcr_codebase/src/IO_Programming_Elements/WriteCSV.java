afe88b6e2663de4147d710ebe4420ca184c2f5b0
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2026-01-20 12:16:49 +0530

    [Anmol Tiwari]:Completed the basic questions

---
+++ IO-Programming-Java/src/gcr_codebase/src/IO_Programming_Elements/WriteCSV.java
@@ -0,0 +1,34 @@
+package IO_Programming_Elements;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class WriteCSV {
+	public static void main(String[] args) {
+		
+		String filepath="C:\\Users\\Anmol Tiwari\\eclipse-workspace\\gcr_codebase\\src\\csv_handling\\employee.csv";
+		
+		try(BufferedWriter bw =new BufferedWriter(new FileWriter(filepath))) {
+			
+			bw.write("ID,Name,Department,Salary");
+			bw.newLine();
+			
+			bw.write("1,Anmol,IT,300000");
+			bw.newLine();
+			bw.write("2,Pranav,IT,500000");
+			bw.newLine();
+			bw.write("3,Amit,IT,340000");
+			bw.newLine();
+			bw.write("4,Parv,IT,330000");
+			bw.newLine();
+			bw.write("5,Praveen,IT,310000");
+			
+			bw.close();
+			
+			System.out.println("File created");
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
