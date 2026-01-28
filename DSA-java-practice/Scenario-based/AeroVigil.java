d8cc4cafc3117a93abbd9a75d0d10a35b9ef22ac
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2026-01-28 16:19:44 +0530

    Create AeroVigil.java

---
+++ DSA-java-practice/Scenario-based/AeroVigil.java
@@ -0,0 +1,18 @@
+public class AeroVigil {
+    public static void main(String[] args){
+        System.out.println(validateFlightNumber("FL-1000"));
+    }
+
+    public static boolean validateFlightNumber(String flightNumber){
+     
+            String f=flightNumber.substring(0,2);
+            if(f.equals("FL")){
+                String n=flightNumber.substring(3,flightNumber.length());
+                int num=Integer.parseInt(n);
+                
+                if(num>1000 && num<=9999)
+                    return true;
+            }
+            return false;
+    }
+}
